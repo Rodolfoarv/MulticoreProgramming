ab05d1e77656593d26262f27a15454ddc8a767b4
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2017-02-01 21:22:32 -0600

    Added fork implementation

--- 3.0 ForkJoin/src/mx/itesm/FactorialTask.java
+++ 3.0 ForkJoin/src/mx/itesm/FactorialTask.java
@@ -6,9 +6,15 @@ import java.util.concurrent.RecursiveTask;
 public class FactorialTask extends RecursiveTask<BigInteger> {
 	
 	// It is recommended to use these two 
+	// FOrk join will divide the problem in many subsets ofthe problem, usually a half.
+	
 	
 	private final int start;
 	private final int end;
+	
+	// We define a threshold
+	
+	private static final int UMBRAL = 1000;
 	
 	public FactorialTask(int start, int end) {
 		this.start = start;
@@ -17,8 +23,23 @@ public class FactorialTask extends RecursiveTask<BigInteger> {
 
 	@Override
 	protected BigInteger compute() {
-		// TODO Auto-generated method stub
-		return null;
+		if (start - end < UMBRAL){
+			BigInteger resultado = BigInteger.ONE;
+			for (int i = start; i < end; i++) {
+				resultado = resultado.multiply(BigInteger.valueOf(i));
+			}
+			return resultado;
+		}else {
+			int mid = (start + end) >>> 2;
+			FactorialTask t1,t2;
+			t1 = new FactorialTask(start, mid);
+			t2 = new FactorialTask(mid, end);
+			
+			// Concurrent
+			t1.fork(); // Fork runs it in another thread 
+			return t2.compute();
+ 			
+		}
 	}
 
 }
