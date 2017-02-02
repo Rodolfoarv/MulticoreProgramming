a683c5991617854520c0059c66955a460e2a4f87
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2017-02-01 21:54:38 -0600

    Finished factorial implementation with Fork-Join

--- 3.0 ForkJoin/src/mx/itesm/FactorialTask.java
+++ 3.0 ForkJoin/src/mx/itesm/FactorialTask.java
@@ -1,6 +1,7 @@
 package mx.itesm;
 
 import java.math.BigInteger;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class FactorialTask extends RecursiveTask<BigInteger> {
@@ -23,23 +24,36 @@ public class FactorialTask extends RecursiveTask<BigInteger> {
 
 	@Override
 	protected BigInteger compute() {
-		if (start - end < UMBRAL){
+		if (end - start < UMBRAL){
 			BigInteger resultado = BigInteger.ONE;
 			for (int i = start; i < end; i++) {
 				resultado = resultado.multiply(BigInteger.valueOf(i));
 			}
 			return resultado;
 		}else {
-			int mid = (start + end) >>> 2;
+			int mid = (start + end) >>> 1;
 			FactorialTask t1,t2;
 			t1 = new FactorialTask(start, mid);
 			t2 = new FactorialTask(mid, end);
 			
 			// Concurrent
 			t1.fork(); // Fork runs it in another thread 
-			return t2.compute();
- 			
+			return t2.compute().multiply(t1.join()); // The join is part of the object Factorial Task, which is Recursive Task 			
 		}
+	}
+	
+	public static void main(String[] args) {
+        long timeStart = System.currentTimeMillis();
+		int n = 1_000_000;
+		
+		FactorialTask task = new FactorialTask(2, n+1);
+		ForkJoinPool pool = new ForkJoinPool();
+		BigInteger resultado = pool.invoke(task);
+		
+        long timeEnd = System.currentTimeMillis();
+
+        System.out.printf("Resultado = %d, tiempo = %.4f", resultado.bitCount(), (timeEnd - timeStart) / 1000.0);
+		
 	}
 
 }
